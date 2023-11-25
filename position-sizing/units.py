6c9efa8007fb3aeb9301d35f36c6850f65b9693a
Gabe Kutner <108239517+gabekutner@users.noreply.github.com>
2023-11-25 10:44:37 -0800

    11.25 update
    Update units.py

--- position-sizing/units.py
+++ position-sizing/units.py
@@ -17,11 +17,36 @@ formula:
   or
   Unit = (1% of Account) / N * Dollars per Point
 """
+from yahoo_fin.stock_info import get_live_price
 
+# first, find Market Dollar Volatility
+  # futures contract: the change in the asset for a 1$ change in the underlying commodity
+  # stock: price per share of the stock
+VERSIONS = {0.1, 1.1}
+def dollar_volatility(version, asset, n):
+  if version not in VERSIONS:
+    raise ValueError("version must be one of %r." % VERSIONS)
+    
+  if version == 0.1:
+    # futures
+    pass
+  if version == 1.1:
+    # stock
+    pps = get_live_price(asset)
+    return n * pps
+    
 
+# second, find unit size
+def unit(dollar_volatility, account_size):
+  return (0.01 * account_size) / dollar_volatility
 
 
 if __name__ == '__main__':
-    # print("Enter the account size ($): ")
-    inp = input("Enter the account size ($): ")
-    print(inp)
+  asset = input("Enter the asset symbol: ")
+  version = input("Enter 0.1 if the asset is a futures contract or 1.1 for common stock: ") 
+  account = input("Enter the account size ($): ")
+  
+  
+
+  
+  

0c6608669e89229e856a92ab2d08e66796977e29
Gabe Kutner <108239517+gabekutner@users.noreply.github.com>
2023-11-25 10:58:42 -0800

    Update main.py

--- position-sizing/main.py
+++ position-sizing/main.py
@@ -2,3 +2,12 @@
 """
 from n import calc_n
 from units import dollar_volatility, unit
+
+
+if __name__ == "__main__":
+  asset = input("Enter a symbol: ")
+  type = input("Enter 0.1 for futures, 1.1 for common stock: ")
+ 
+  N = calc_n(asset)
+  D = dollar_volatility(version=float(type), asset=asset, n=N)
+  unit_size = unit()

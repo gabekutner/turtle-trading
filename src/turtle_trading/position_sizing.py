9cbe8607939ea0834bcef6ac9fdc22c858ab8631
gabekutner <108239517+gabekutner@users.noreply.github.com>
2023-12-04 15:29:57 -0700

    update 12.4 8>? 9?

--- src/turtle_trading/position_sizing.py
+++ src/turtle_trading/position_sizing.py
@@ -1,6 +1,34 @@
-# Class for determining volatility adjust position units.
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+"""https://oxfordstrat.com/coasdfASD32/uploads/2016/01/turtle-rules.pdf
+
+Position sizing is one of the most important but least understood components
+of any trading system.
+
+The Turtles used a position sizing algorithm that was very advanced for its day,
+because it normalized the dollar volatility of a position by adjusting the position size
+based on the dollar volatility of the market. This meant that a given position would
+tend to move up or down in a given day about the same amount in dollar terms (when
+compared to positions in other markets), irrespective of the underlying volatility of the
+particular market.
+
+This is true because positions in markets that moved up and down a large amount per
+contract would have an offsetting smaller number of contracts than positions in
+markets that had lower volatility.
+
+This volatility normalization is very important because it means that different trades in
+different markets tend to have the same chance for a particular dollar loss or a
+particular dollar gain. This increased the effectiveness of the diversification of trading
+across many markets.
+
+Even if the volatility of a given market was lower, any significant trend would result in
+a sizeable win because the Turtles would have held more contracts of that lower
+volatility commodity.
+"""
+
 import math
 import pandas as pd
+
 from yahoo_fin.stock_info import get_data, get_live_price
 
 
@@ -8,19 +36,11 @@ class Unit(object):
   """A Volatitility Adjusted Position Unit. 
   Args:
     asset: The ticker or symbol of an asset.
-    version: 0.1 for futures, 1.1 for common stock
+    account_size: The total value of the account.
   """
-  def __init__(self, asset: str, account_size: float, version: float = 1.1) -> None:
-    """"""
+  def __init__(self, asset: str, account_size: float, *args, **kwargs) -> None:
     self.asset = asset
     self.account_size = account_size
-
-    VERSIONS = {0.1, 1.1}
-    if version in VERSIONS:
-      self.version = version
-    else:
-      raise ValueError("version must be one of %r" % VERSIONS)
-        
     self.__process()
     
   def __process(self):
@@ -33,9 +53,14 @@ class Unit(object):
   def _get_dataframe(self) -> pd.DataFrame:
     """Add true range column to dataframe."""
     df = get_data(ticker=self.asset, interval='1d')
+
     dataframe = df[['low', 'high', 'close']].tail(21)
     dataframe['previous_close'] = dataframe['close'].shift(1)
-    dataframe['true_range'] = dataframe['high'] - dataframe['low']
+    dataframe['true_range'] = 0
+
+    for index, row in enumerate(dataframe.iterrows()):
+      curr = dataframe.iloc[index]
+      dataframe.iloc[index, dataframe.columns.get_loc('true_range')] = max(curr['high'] - curr['low'], curr['high'] - curr['previous_close'], curr['previous_close'] - curr['low'])
 
     self.dataframe = dataframe
     return dataframe
@@ -49,22 +74,22 @@ class Unit(object):
     dataframe = self._get_dataframe()
     pdn = self._pdn()
     true_range = dataframe['true_range'][-1]
-    self.N = (19 * pdn + true_range) / 20
 
+    self.N = (19 * pdn + true_range) / 20
     return self.N
 
   def _dollar_volatility(self) -> float:
-    """Calculate the market dollar volatility."""
-    if self.version == 0.1:
-      # futures
-      print("Futures feature not finished.")
-    
-    if self.version == 1.1:
-      pps = get_live_price(self.asset)
-      return self.N * pps
+    """Calculate the market dollar volatility of the asset."""
+    # add in functionality for futures contract.
+    pps = get_live_price(self.asset)
+    return self.N * pps
     
   def get_unit(self) -> float:
     """Calculate the volatility adjusted unit size."""
     dollar_volatility = self._dollar_volatility()
     self.unit = math.trunc((0.01 * self.account_size) / dollar_volatility)
     return self.unit
+
+if __name__ == '__main__':
+  unit = Unit(asset="AAPL", account_size=1_000_000.00)
+  print(unit)

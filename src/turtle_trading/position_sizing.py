269cba90845b9b067d4c73c2f31155c220b5db44
gabekutner <108239517+gabekutner@users.noreply.github.com>
2023-12-04 13:05:05 -0700

    12. 3 update 6?

--- src/turtle_trading/position_sizing.py
+++ src/turtle_trading/position_sizing.py
@@ -1,5 +1,6 @@
 # Class for determining volatility adjust position units.
 import math
+import pandas as pd
 from yahoo_fin.stock_info import get_data, get_live_price
 
 
@@ -29,7 +30,7 @@ class Unit(object):
   def __str__(self):
     return f"{self.asset}: {self.unit}"
 
-  def _get_dataframe(self):
+  def _get_dataframe(self) -> pd.DataFrame:
     """Add true range column to dataframe."""
     df = get_data(ticker=self.asset, interval='1d')
     dataframe = df[['low', 'high', 'close']].tail(21)

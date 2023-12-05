81c8dcae87b6509864f68565a972c78d73d295ed
gabekutner <108239517+gabekutner@users.noreply.github.com>
2023-12-04 22:39:14 -0700

    update 12.4 #10

--- src/turtle_trading/position_sizing/adjusting_size.py
+++ src/turtle_trading/position_sizing/adjusting_size.py
@@ -1,5 +1,42 @@
-"""https://
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+"""https://oxfordstrat.com/coasdfASD32/uploads/2016/01/turtle-rules.pdf
+
 Adjusting Trading Size
+
+There will be times when the market does not trend for many months. During these
+times, it is possible to lose a significant percentage of the equity of the account.
+After large winning trades close out, one might want to increase the size of the equity
+used to compute position size.
+
+The Turtles did not trade normal accounts with a running balance based on the initial
+equity. We were given notional accounts with a starting equity of zero and a specific
+account size. For example, many Turtles received a notional account size of $1,000,000
+when we first started trading in February, 1983. This account size was then adjusted
+each year at the beginning of the year. It was adjusted up or down depending on the
+success of the trader as measured subjectively by Rich. The increase/decrease typically
+represented something close to the addition of the gains or losses that were made in
+the account during the preceding year.
+
+The Turtles were instructed to decrease the size of the notional account by 20% each
+time we went down 10% of the original account. So if a Turtle trading a $1,000,000
+account was ever was down 10%, or $100,000, we would then begin trading as if we
+had a $800,000 account until such time as we reached the yearly starting equity. If we
+lost another 10% (10% of $800,000 or $80,000 for a total loss of $180,000) we were to
+reduce the account size by another 20% for a notional account size of $640,000.
+
+There are other, perhaps better strategies for reducing or increasing equity as the
+account goes up or down. These are simply the rules that the Turtles used
+"""
 
 
 """
+The Turtles were instructed to decrease the size of the notional account by 20% each
+time we went down 10% of the original account. So if a Turtle trading a $1,000,000
+account was ever was down 10%, or $100,000, we would then begin trading as if we
+had a $800,000 account until such time as we reached the yearly starting equity
+"""
+def decrease_size(account_size: float, ) -> float:
+  """Return the new account size. This function is called if the account has suffered a 10% loss."""
+  loss = 0.2 * account_size
+  return account_size - loss
--- src/turtle_trading/position_sizing/risk_management.py
+++ src/turtle_trading/position_sizing/risk_management.py
@@ -1,10 +1,25 @@
-"""https://
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+"""https://oxfordstrat.com/coasdfASD32/uploads/2016/01/turtle-rules.pdf
+
 Units as a measure of Risk
+
+Since the Turtles used the Unit as the base measure for position size, and since those
+units were volatility risk adjusted, the Unit was a measure of both the risk of a position,
+and of the entire portfolio of positions.
 
 The Turtles were given risk management rules that limited the number of Units that
 we could maintain at any given time, on four different levels. In essence, these rules
 controlled the risk that a trader could carry, and these limits minimized losses
 during prolonged losing periods, as well as during extraordinary price movements.
+
+An example of an extraordinary price movement was the day after the October, 1987
+stock market crash. The U.S. Federal Reserve lowered interest rates by several
+percentage points overnight to boost the confidence of the stock market and the
+country. The Turtles were loaded long in interest rate futures: Eurodollars, TBills and
+Bonds. The losses the following day were enormous. In some cases, 20% to 40% of
+account equity was lost in a single day. But these losses would have been
+correspondingly higher without the maximum position limits.
 
 The limits were: 
 Level    Type                                Maximum Units
@@ -13,3 +28,34 @@ Level    Type                                Maximum Units
 3        Loosely Correlated Markets          10 units
 4        Single Direction -- Long or Short   12 units
 """
+
+
+"""Single Markets - A maximum of four Units per market."""
+class SingleMarketsException(Exception):
+    pass
+
+"""Closely Correlated Markets -
+For markets that were closely correlated and there could be
+a maximum of 6 Units in one particular direction (i.e. 6 long units or 6 short units).
+Closely correlated markets include: heating oil and crude oil; gold and silver; Swiss
+franc and Deutschmark; TBill and Eurodollar, etc.
+"""
+class CloselyCorrelatedMarketsException(Exception):
+    pass
+
+"""Loosely Correlated Markets - 
+For loosely correlated markets, there could be a
+maximum of 10 Units in one particular direction. Loosely correlated markets included:
+gold and copper; silver and copper, and many grain combinations that the Turtles did
+not trade because of positions limits.
+"""
+class LooselyCorrelatedMarketsException(Exception):
+    pass
+
+"""Single Direction - 
+The maximum number of total Units in one direction long or short
+was 12 Units. Thus, one could theoretically have had 12 Units long and 12 Units short
+at the same time.
+"""
+class SingleDirectionException(Exception):
+    pass

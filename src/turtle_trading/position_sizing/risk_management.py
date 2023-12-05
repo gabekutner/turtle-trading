d061ce766838d44c915735efce41d39f37df9996
Gabe Kutner <108239517+gabekutner@users.noreply.github.com>
2023-12-04 17:29:41 -0700

    Update risk_management.py

---
+++ src/turtle_trading/position_sizing/risk_management.py
@@ -0,0 +1,15 @@
+"""https://
+Units as a measure of Risk
+
+The Turtles were given risk management rules that limited the number of Units that
+we could maintain at any given time, on four different levels. In essence, these rules
+controlled the risk that a trader could carry, and these limits minimized losses
+during prolonged losing periods, as well as during extraordinary price movements.
+
+The limits were: 
+Level    Type                                Maximum Units
+1        Single Market                       4 units
+2        Closely Correlated Markets          6 units
+3        Loosely Correlated Markets          10 units
+4        Single Direction -- Long or Short   12 units
+"""

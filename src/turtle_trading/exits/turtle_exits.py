d8bb588bb5c53bf4f2bd0624086be3247d541470
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-01-05 11:12:41 -0800

    update

--- src/turtle_trading/exits/turtle_exits.py
+++ src/turtle_trading/exits/turtle_exits.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 """  """
+import datetime
 import pandas as pd
 from typing import Union, Literal
 
-from yahoo_fin.stock_info import get_data
+from turtle_trading.dataframe_loader import DataFrameLoader
 
 
-def price_against_position(dataframe: pd.DataFrame, day: int, unit_price: Union[float, list[float]]):
+def price_against_position(dataframe: DataFrameLoader, day: int, date_entered: datetime.date):
   """  """
-  pass
+  dataframe.reset()
+
+  dataframe.start_at(date_entered)
+
+  for index, row in enumerate(dataframe.dataframe.iterrows()):
+    if index+1 == day:
+      return False
+    
